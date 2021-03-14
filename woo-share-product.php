67cff8a3d4987b0a6b8ed0fb4a3893bd219e154d
Harun <harunalrashyid@gmail.com>
2021-03-14 22:48:17 +0700

    update - plugin setting link

--- woo-share-product.php
+++ woo-share-product.php
@@ -15,6 +15,9 @@ if ( !defined('ABSPATH') ) {
   die('-1');
 }
 
+$plugin_file = plugin_basename(__FILE__);
+
+define( 'WOOSP_PLUGIN_FILE', $plugin_file );
 define('WOOSP_VERSION', '1.0.0');
 
 require_once( __DIR__ . '/includes/woo_share_product.php' );
