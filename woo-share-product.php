96add8510abab171b8bb2ec9e72ca9ee7b1f199b
Harun <harunalrashyid@gmail.com>
2021-03-14 14:13:26 +0700

    initial commit

---
+++ woo-share-product.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Woocommerce Share Product
+ * Description: Add share button social into your single product
+ * Version: 1.0.0
+ * Author: Harun
+ * Author URI: https://github.com/harunalrashyid
+ */
+
+if ( !defined('ABSPATH') ) {
+  die('-1');
+}
